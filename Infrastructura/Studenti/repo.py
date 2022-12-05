15be100e1858f3ed007e75b0fc696a49e275838b
BuildTools <unconfigured@null.spigotmc.org>
2022-12-05 15:46:16 +0200

    Fisier pentru elemente de tip student

--- Infrastructura/Studenti/repo.py
+++ Infrastructura/Studenti/repo.py
@@ -65,3 +65,74 @@ class StudentRepo():
 
         if id in self.__lista:
             del(self.__lista[id])
+
+
+class StudentRepoFisiere(StudentRepo):
+    def __init__(self):
+       StudentRepo.__init__(self)
+
+       self.__path = r'C:\Users\Gabi\Desktop\Lab_7_clase\studenti.txt'
+       self.__load_from_file()
+    
+    def get_list(self):
+        self.__load_from_file()
+        return StudentRepo.get_list(self)
+
+    def adauga_student_repo(self, student):
+          self.__load_from_file()
+          StudentRepo.adauga_student_repo(self, student)
+          self.__store_in_file()
+
+    def size_student_repo(self):
+        self.__load_from_file()
+        return StudentRepo.size_student_repo(self)
+
+    def cauta_id_student_repo(self, id):
+        self.__load_from_file()
+        return StudentRepo.cauta_id_student_repo(self, id)
+
+    def modificare_id_student_repo(self, id, student_change):
+          self.__load_from_file()
+          StudentRepo.modificare_id_student_repo(self, id, student_change)
+          self.__store_in_file()
+
+    def delete_id_student_repo(self, id):
+          self.__load_from_file()
+          StudentRepo.delete_id_student_repo(self, id)
+          self.__store_in_file()
+
+
+
+    def __load_from_file(self):
+        """
+        functia resposnabila pentru a adauga in lista de studenti, studentii din fisier
+        """
+        StudentRepo.__init__(self)
+
+        try:
+         fisier_studenti = open(self.__path, "r")
+        except IOError:
+            pass
+
+        studentul_impachetat = fisier_studenti.readline()
+        while studentul_impachetat != "":
+            studentul_impachetat  =studentul_impachetat.strip()
+            studentul_params = studentul_impachetat.split(";")
+            StudentRepo.adauga_student_repo(self, Student(studentul_params[0],studentul_params[1]))
+            studentul_impachetat = fisier_studenti.readline()
+        fisier_studenti.close()
+
+
+    def __store_in_file(self):
+        """
+        Functia responsabila pentru a updata fisierul cand apar elemente noi
+        """
+        try:
+         fisier_studenti = open(self.__path, "w")
+        except IOError:
+            pass
+        lista_studenti = StudentRepo.get_list(self)
+
+        for student in lista_studenti:
+            fisier_studenti.write(f"{student.get_id()};{student.get_nume()}\n")
+        fisier_studenti.close()
